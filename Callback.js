c5f48b48397c43fbb2805e2cf5a21158e9e15411
asus test <msa-99@outlook.com>
2024-01-08 21:32:35 +0600

    callack control flow

--- Callback.js
+++ Callback.js
@@ -11,19 +11,47 @@
 // console.log('Completed order for coustomer 1');
 
 // eta asynchronous code, ekhane kuno blocking behavior thake na
-const processOrder=(coustomer)=>{
-        console.log('Processing order for coustomer 1');
+// const processOrder=(coustomer)=>{
+//         console.log('Processing order for coustomer 1');
 
-        setTimeout(()=>{
-            console.log('cooking completed');
-            },3000)
+//         setTimeout(()=>{
+//             console.log('cooking completed');
+//             },3000)
 
-       console.log('Order processed for coustomer 1');
-     }
-     
-console.log('Take order for coustomer 1');
-processOrder('Coustomer 1')
- console.log('Completed order for coustomer 1');
+//        console.log('Order processed for coustomer 1');
+//      }
+
+// console.log('Take order for coustomer 1');
+// processOrder('Coustomer 1')
+//  console.log('Completed order for coustomer 1');
+
+//  cntrolled code exicution flow by callback function
+const takeOrder=(coustomer, callback)=>{
+console.log(`Take order for ${coustomer}`)
+callback(coustomer)
+}
+
+const processingOrder=(coustomer,callback)=>{
+console.log(`Processing order for ${coustomer}`)
+setTimeout(()=>{
+console.log('Cooking completed!');
+callback(coustomer)
+},3000)
+}
+
+const orderStatus=(coustomer)=>{
+console.log(`Order completed for ${coustomer}`);
+}
+
+takeOrder('MSA',(coustomer)=>{
+    processingOrder(coustomer,(coustomer)=>{
+    orderStatus(coustomer)
+    })
+})
+console.log('Hello');
+
+
+// takeOrder('MSA',processingOrder('MSA',orderStatus('MSA')))
 
 
 
