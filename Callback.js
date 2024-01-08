96241c1b307a765c1624bb652c2f4c94f265d8fa
asus test <msa-99@outlook.com>
2024-01-08 20:26:32 +0600

    asynchronous non blocking behaviour

--- Callback.js
+++ Callback.js
@@ -1,1 +1,29 @@
-console.log('MSA');
+// eta synchronous code ebong while loop er jonno browser 3 sec block hoye thake.
+// browser block hoye takhar jonno user kuno dhoroner interactivity korte pare na.
+// const processOrder=(coustomer)=>{
+//     console.log('Processing order for coustomer 1');
+//     var currentTime=new Date().getTime()
+//     while (currentTime + 3000 >= new Date().getTime())
+//     console.log('Order processed for coustomer 1');
+// }
+// console.log('Take order for coustomer 1');
+// processOrder('Coustomer 1')
+// console.log('Completed order for coustomer 1');
+
+// eta asynchronous code, ekhane kuno blocking behavior thake na
+const processOrder=(coustomer)=>{
+        console.log('Processing order for coustomer 1');
+
+        setTimeout(()=>{
+            console.log('cooking completed');
+            },3000)
+
+       console.log('Order processed for coustomer 1');
+     }
+     
+console.log('Take order for coustomer 1');
+processOrder('Coustomer 1')
+ console.log('Completed order for coustomer 1');
+
+
+
