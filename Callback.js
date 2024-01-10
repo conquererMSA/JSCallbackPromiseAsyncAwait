1330a40140fa935d6a812c9b98d8612de5cf9c5e
asus test <msa-99@outlook.com>
2024-01-10 14:57:48 +0600

    callbacj w3 basic

--- Callback.js
+++ Callback.js
@@ -1,3 +1,54 @@
+// Basic callback function
+
+// function display(result){
+//     const display=document.getElementById('value')
+//     display.innerText=`value is: ${result}`
+// }
+
+// function calculate(num1, num2){
+// const sum= num1+num2 
+// display(sum)
+
+// }
+// calculate(2,4)
+
+// function calculate(num1,num2, callback){
+//     const result=num1+num2 
+//     if(callback) callback(result)
+//     return result
+// }
+
+// function display(result){
+//     console.log(result);
+    
+// }
+
+// const result=calculate(2,4)
+// console.log(result);
+
+// const calculate=(num1,num2,display)=>{
+//     const sum=num1+num2 
+//     if(display)display(sum)
+// }
+// // const display=(value)=>{
+// //     console.log(value);
+// //     }
+// calculate(2,5,(value)=>{
+//     console.log(value);
+//     })
+
+const calculate=(num1,num2, callback)=>{
+    if(callback)callback('its will call later') //wrong
+    const sum=num1+num2 
+    console.log('its call first',sum);
+    }
+calculate(5,5,(strValue)=>{
+    console.log(strValue);
+})
+
+
+
+
 // eta synchronous code ebong while loop er jonno browser 3 sec block hoye thake.
 // browser block hoye takhar jonno user kuno dhoroner interactivity korte pare na.
 // const processOrder=(coustomer)=>{
@@ -26,29 +77,29 @@
 //  console.log('Completed order for coustomer 1');
 
 //  cntrolled code exicution flow by callback function
-const takeOrder=(coustomer, callback)=>{
-console.log(`Take order for ${coustomer}`)
-callback(coustomer)
-}
+// const takeOrder=(coustomer, callback)=>{
+// console.log(`Take order for ${coustomer}`)
+// callback(coustomer)
+// }
 
-const processingOrder=(coustomer,callback)=>{
-console.log(`Processing order for ${coustomer}`)
-setTimeout(()=>{
-console.log('Cooking completed!');
-callback(coustomer)
-},3000)
-}
+// const processingOrder=(coustomer,callback)=>{
+// console.log(`Processing order for ${coustomer}`)
+// setTimeout(()=>{
+// console.log('Cooking completed!');
+// callback(coustomer)
+// },3000)
+// }
 
-const orderStatus=(coustomer)=>{
-console.log(`Order completed for ${coustomer}`);
-}
+// const orderStatus=(coustomer)=>{
+// console.log(`Order completed for ${coustomer}`);
+// }
 
-takeOrder('MSA',(coustomer)=>{
-    processingOrder(coustomer,(coustomer)=>{
-    orderStatus(coustomer)
-    })
-})
-console.log('Hello');
+// takeOrder('MSA',(coustomer)=>{
+//     processingOrder(coustomer,(coustomer)=>{
+//     orderStatus(coustomer)
+//     })
+// })
+// console.log('Hello');
 
 
 // takeOrder('MSA',processingOrder('MSA',orderStatus('MSA')))
---
+++ Promise.js
@@ -0,0 +1,47 @@
+// Promise kuno ekta condition er upor kaj kore. condition fulfill hole promise resolved hoy and condition fail hole promise rejected hoy.
+// Promise asynchronously exicute hoy
+// Promise  diye kuno ekta code block er exicution tamiye deya zay.
+
+const hasMeeting=false
+const meeting=new Promise((resolved,rejected)=>{
+if(!hasMeeting){
+    const meeting={
+    name:'Technical Meeting',
+    location:'Zoom',
+    time:'10AM'
+    }
+    resolved(meeting)
+}else{
+    rejected(new Error('Meeting has been scheduled!'))
+}
+})
+
+const addToCalender=(meetingDetails)=>{
+const details=`${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`
+return Promise.resolve(details)
+}
+// const proValue=addToCalender({
+//     name:'Technical Meeting',
+//     location:'Zoom',
+//     time:'10AM'
+//     })
+// console.log('proValue',proValue);
+    
+// ekhane uporer meeting promise zeta resolve korbe seti addToCalender function recieve korbe ebong addToCalender function ze promise resolve korbe seti 2nd then block e recieve korbo.
+// meeting.then(addToCalender).then((res)=>{
+// console.log(res)
+
+// }).catch((error)=>{
+// console.log(error.message);
+// })
+
+// multiple promise 
+const promise1=Promise.resolve('Promise 1 resolved')
+const promise2=new Promise((resolved,rejected)=>{
+    setTimeout(()=>{
+     resolved('Promise 2 resolved')
+    },3000)
+})
+
+// Promise.all([promise1,promise2]).then(res=>console.log(res))
+Promise.race([promise1,promise2]).then(res=>console.log(res))
